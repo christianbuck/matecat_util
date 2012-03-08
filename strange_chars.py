c9f0106db8cd0b34c76946755b6b5cfeb8d2b7c9
Christian Buck <cbuck@lantis.de>
2012-03-08 20:06:11 +0000

    report unicode whitespaces
    added option to ignore tabs
    added uniq option to print chars only once

--- strange_chars.py
+++ strange_chars.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 from itertools import imap, izip
 import codecs
 from writer import write_numbers
+import re
 
 if __name__ == "__main__":
     import argparse
@@ -15,6 +16,10 @@ if __name__ == "__main__":
                         help="maximum number of 'strange' characters (default: 3)")
     parser.add_argument('-v', action='store_true', dest='verbose',
                         help='verbose (default:off)', default=False)
+    parser.add_argument('-i', '-ignore_whitespace', action='store_true', dest="no_ws",
+                        help='ignore all whitespace characters including tabs', default=False)
+    parser.add_argument('-u', '-uniq', action='store_true', dest="uniq",
+                        help='report each character only once', default=False)
     args = parser.parse_args(sys.argv[1:])
 
     # english standard characters
@@ -50,16 +55,24 @@ if __name__ == "__main__":
     in_stream = codecs.getreader("utf-8")(sys.stdin)   # read from stdin
     out_stream = codecs.getwriter("utf-8")(sys.stdout) # write to stdout
 
+    re_whitespace = re.compile("\s+")
 
     strange_lines = []
     for linenr, line in enumerate(in_stream):
         line = line.strip()
+        if args.no_ws:
+            line = re_whitespace.sub(line, " ")
         # strange_chars = set(line) - chars
         strange_chars = [c for c in line if not c in chars]
         if len(strange_chars) > 3:
+            #print strange_chars
             if args.verbose:
-                out_stream.write(u"line %s: offending characters: %s\n"
-                                 %(linenr, u" ".join(strange_chars)))
+                out_stream.write(u"line %s offending characters:" %(linenr))
+                if args.uniq:
+                    strange_chars = set(strange_chars)
+                for c in strange_chars:
+                    out_stream.write(u" %s (%s)" %(c, repr(c)))
+                out_stream.write(u"\n")
                 out_stream.write(line + u"\n")
             strange_lines.append(linenr)
 
