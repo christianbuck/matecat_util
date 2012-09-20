2b957a16afe73650c82b21f4d80fba50ca85268f
Christian Buck <cbuck@lantis.de>
2012-09-20 17:55:26 +0200

    export text which may again contain tags

--- xliff/xml2txt.py
+++ xliff/xml2txt.py
@@ -2,8 +2,25 @@
 
 import sys
 import xml.etree.ElementTree as ET
+import re
 
-''' extract source text from xliff files '''
+''' extract all text from xml files '''
+
+def peel(s):
+    # remove one layer of tags
+    re_tag = re.compile(r"^<[^>]+>(?P<inner>.*)<[^>]+>$", re.DOTALL)
+    m = re_tag.match(s.strip())
+    if m:
+        return m.groupdict()['inner'].strip()
+
+def get_text(tree):
+    if (tree.text and tree.text.strip()) or (tree.tail and tree.tail.strip()):
+        text = ET.tostring(tree, encoding='utf-8').strip()
+        yield peel(text)
+    else:
+        for child in tree:
+            for text in  get_text(child):
+                yield text
 
 if __name__ == '__main__':
     import argparse
@@ -14,7 +31,5 @@ if __name__ == '__main__':
     tree = ET.parse(args.xml)
     root = tree.getroot()
 
-    for src in root.iter():
-        if src.text and src.text.strip():
-            src_text = src.text.strip().replace("\n",'NEWLINE')
-            print src_text.encode('utf-8')
+    for text in get_text(root):
+        print text
