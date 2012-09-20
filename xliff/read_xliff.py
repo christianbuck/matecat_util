38ce9b3a883069cda53404317e28a55cfa92d362
Christian Buck <cbuck@lantis.de>
2012-09-20 19:06:05 +0200

    extract souces that contain tags and remove namespaces.

--- xliff/read_xliff.py
+++ xliff/read_xliff.py
@@ -2,8 +2,25 @@
 
 import sys
 import xml.etree.ElementTree as ET
+import re
 
 ''' extract source text from xliff files '''
+
+def peel(s):
+    # remove one layer of tags
+    re_tag = re.compile(r"^<[^>]+>(?P<inner>.*)<[^>]+>$", re.DOTALL)
+    m = re_tag.match(s.strip())
+    if m:
+        return m.groupdict()['inner'].strip()
+    else:
+        return s
+
+def remove_ns(s):
+    # in: <ns0:x id="7534" /><ns0:g id="7535">
+    # out: <x id="7534" /><g id="7535">
+    re_ns = re.compile(r'<[^ >]*[:](\w+[ />])')
+    return re_ns.sub("<\g<1>",s)
+
 
 if __name__ == '__main__':
     import argparse
@@ -19,5 +36,6 @@ if __name__ == '__main__':
         if 'translate' in tu.attrib and tu.attrib['translate'] == 'no':
             continue
         for src in tu.iter(prefix + 'source'):
-            if src.text and src.text.strip():
-                print src.text.encode('utf-8')
+            if (src.text and src.text.strip()) or (src.tail and src.tail.strip()):
+                src_text = ET.tostring(src, encoding='utf-8')
+                print remove_ns(peel(src_text))
