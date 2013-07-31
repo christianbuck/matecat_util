2983a08dffdb79c83fc3988d6608cadbaabf5b04
Nicola Bertoldi <bertoldi@fbk.eu>
2013-07-31 17:14:17 +0200

    clean up

--- SERVER_hltservices/url_encode_example/escape.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-use URI::Escape;
-
-while (<STDIN>) {
-  print uri_escape($_), "\n";
-}
--- SERVER_hltservices/url_encode_example/escape.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import urllib
-import sys
-
-for line in sys.stdin:
-    print urllib.quote(line)
